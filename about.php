0df9e3bd232b6617039d29ddf07966f3afc0aefc
Redtide62 <38059024+Redtide62@users.noreply.github.com>
2018-04-18 11:07:49 -0700

    Update about.php
    Added code to fit to device screen

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
     <title>About</title>
 </head>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
